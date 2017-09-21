cca52ae237017d6b23b371e608c58609eeeaf7b9
ColinvD <22950@ma-web.nl>
2017-09-21 09:35:19 +0200

    platform moving last added

---
+++ EndlessRunner/Assets/Scripts/Datas.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Datas : MonoBehaviour {
+
+    public float Speed;
+    private float Acceleration;
+    private int cooldown;
+    private int maxCooldown;
+    // Use this for initialization
+    void Start () {
+        Speed = 1;
+        Acceleration = 1.5f;
+        maxCooldown = 500;
+        cooldown = maxCooldown;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine("Test");
+	}
+
+    private IEnumerator Test()
+    {
+        while (true)
+        {
+            //Debug.Log(cooldown);
+            if (cooldown > 0)
+            {
+                cooldown--;
+            }
+            else if (cooldown <= 0)
+            {
+                Speed += Acceleration;
+                cooldown = maxCooldown;
+                Debug.Log(Speed);
+            }
+            yield return new WaitForSeconds(10);
+        }
+
+    }
+}
---
+++ EndlessRunner/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+    public Datas data;
+    private Rigidbody2D rb2d;
+
+    //[SerializeField]
+    private float mySpeed;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        mySpeed = data.Speed;
+        Debug.Log(mySpeed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 direction = (Vector2.left) * mySpeed * Time.fixedDeltaTime;
+        rb2d.MovePosition(rb2d.position + direction);
+	}
+}
