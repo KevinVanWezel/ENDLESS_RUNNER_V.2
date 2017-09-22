a651578f9d0f05dee19c11396eecde033a95e9ab
ColinvD <22950@ma-web.nl>
2017-09-22 14:45:30 +0200

    Platforms are getting destroyed outside the screen

---
+++ EndlessRunner/Assets/Scripts/DestoryPlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryPlatform : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(this.transform.position.x <= -21)
+        {
+            //Debug.Log("Bang");
+            Destroy(this.gameObject);
+        }
+	}
+}
