600ffebb071603371ba00584c150337d26fee0c3
KevinVanWezel <21973@ma-web.nl>
2017-09-25 14:58:18 +0200

    background_movement_scripts

---
+++ EndlessRunner/Assets/Scripts/Datas_backgorund.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Datas : MonoBehaviour {
+
+    public float Speed;
+    private float Acceleration;
+    private int cooldown;
+    private int maxCooldown;
+    // Use this for initialization
+    void Start () {
+        Speed = 2.5f;
+        //Acceleration = 0.1f;
+        maxCooldown = 1000;
+        cooldown = maxCooldown;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine("SpeedAcceleration");
+	}
+
+    private IEnumerator SpeedAcceleration()
+    {
+        while (true)
+        {
+            //Debug.Log(cooldown);
+            if (cooldown > 0)
+            {
+                cooldown--;
+            }
+            else if (cooldown <= 0)
+            {
+                Speed += Acceleration;
+                cooldown = maxCooldown;
+                Debug.Log(Speed);
+            }
+            yield return new WaitForSeconds(200);
+        }
+
+    }
+}
