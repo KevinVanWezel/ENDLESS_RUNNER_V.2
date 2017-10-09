6ef40f6edf2aa8d50af1179308e9c52072bd6203
ColinvD <22950@ma-web.nl>
2017-10-09 15:01:41 +0200

    Pause bug fixed

--- EndlessRunner/Assets/Scripts/player_jumping.cs
+++ EndlessRunner/Assets/Scripts/player_jumping.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class player_jumping : MonoBehaviour {
 
-
-	// Use this for initialization
-	void Start () {
+    public Pause stop;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Debug.Log("Space");
-            rb.AddForce(Vector2.up * 20);
-        }
-            
+            if (Input.GetKey(KeyCode.Space) && stop.stopEverything == false)
+            {
+                Debug.Log("Space");
+                rb.AddForce(Vector2.up * 20);
+            }
     }
 }
