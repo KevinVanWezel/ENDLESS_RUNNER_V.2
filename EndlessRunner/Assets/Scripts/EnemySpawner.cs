043ad526dca9c53c2b2e582e91a4f8edaa6e19eb
KevinVanWezel <21973@ma-web.nl>
2017-10-11 11:09:05 +0200

    score

--- EndlessRunner/Assets/Scripts/EnemySpawner.cs
+++ EndlessRunner/Assets/Scripts/EnemySpawner.cs
@@ -1,33 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour {
     
     public List<GameObject> Enemy;
     public Transform EnemySpawnplace;
-    public Transform followerPlace;
-    public AntiPlayerCamping follower;
     [SerializeField]
     private float spawntime = 3;
-    private float nextEnemy;
+    private float nextPlatform;
     private float spawnbereik;
-
+    public Text ScoreText;
+    public int score;
     // Update is called once per frame
+     void Start()
+    {
+       // score = 0;
+        UpdateScore();
+    }
     void Update () {
-        spawnbereik = Random.Range(-4f, 4f);
+        spawnbereik = Random.Range(-4f, 3f);
         Vector3 position = new Vector3(EnemySpawnplace.position.x, spawnbereik, -1);
-        if (Time.time > nextEnemy)
+        if (Time.time > nextPlatform)
         {
-            nextEnemy = Time.time + spawntime;
-            if(follower.FocusShot == true)
-            {
-                position = new Vector3(EnemySpawnplace.position.x, followerPlace.position.y, -1);
-                Instantiate(Enemy[Random.Range(0, 2)], position, EnemySpawnplace.rotation);
-                follower.FocusShot = false;
-                follower.timer = 0;
-            }
-            else if (spawnbereik <= -2f)
+            nextPlatform = Time.time + spawntime;
+            if (spawnbereik <= -2f)
             {
                 Instantiate(Enemy[2], position, EnemySpawnplace.rotation);
             }
@@ -37,4 +35,14 @@ public class EnemySpawner : MonoBehaviour {
             }
         }
 	}
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+    void UpdateScore ()
+    {
+        ScoreText.text = "score: " + score;
+    }
 }
+
